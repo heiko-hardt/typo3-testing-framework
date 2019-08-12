a0e3dafa3e9bae95d5c2ca984e8b701667eec21f
Heiko Hardt <software.heiko@hardt.me>
2019-08-12 20:36:03 +0200

    [TASK] Adding FunctionalTestCaseBootstrapUtility from TYPO3 project
    This utility class was needed for TYPO3 v.6 & v.7 environment

---
+++ src/TestCaseBootstrapUtility.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace HeikoHardt\TYPO3TestingFramework;
+
+use HeikoHardt\TYPO3TestingFramework\Resources\TestCaseBootstrapUtilityV62;
+
+/**
+ * Utility class to set up and bootstrap TYPO3 CMS for functional tests
+ */
+class TestCaseBootstrapUtility extends TestCaseBootstrapUtilityV62
+{
+}
